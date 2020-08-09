de79991de119fe08cc4dae44095d1179453b5e4c
Ephraim Ruttenberg <ephraim408@gmail.com>
2020-08-09 02:14:04 -0400

    Added downsizing to remove(), added tests and documentation, changed tuple dict sizing

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -32,7 +32,6 @@ pub struct Dictionary<K: Clone + Hash, V: Clone> {
     table: Vec<Bucket<K, V>>
 }
 
-
 impl<K: Clone + Hash + PartialEq, V: Clone> Dictionary<K, V>{
     pub fn new() -> Dictionary<K, V> {
         Dictionary {
@@ -42,7 +41,7 @@ impl<K: Clone + Hash + PartialEq, V: Clone> Dictionary<K, V>{
         }
     }
 
-    pub fn new_sized(size: usize) -> Dictionary<K, V> {
+    pub fn with_capacity(size: usize) -> Dictionary<K, V> {
         if size == 0 {
             panic!("Cannot create a zero-sized dict");
         }
@@ -53,105 +52,12 @@ impl<K: Clone + Hash + PartialEq, V: Clone> Dictionary<K, V>{
             table: vec![Bucket::Empty; size]
         }
     }
-    
-
-    //Returns a Result::Err if the vectors are different sizes
-    pub fn from_vecs(mut key_vec: Vec<K>, mut value_vec: Vec<V>) -> Dictionary<K, V> {
-        if key_vec.len() != value_vec.len() {
-            panic!("Differently sized vecs");
-        } else if key_vec.is_empty() {
-            panic!("Cannot create a zero-sized dict");
-        } else {
-            let _size = (key_vec.len()/2)*3 + 1;
-            let mut dict: Dictionary<K, V> = Dictionary { 
-                capacity: _size,
-                size: 0,
-                table: vec![Bucket::Empty; _size]
-            };
-            for _ in 0..key_vec.len() {
-                let key = key_vec.pop().unwrap();
-                let value = value_vec.pop().unwrap();
-                dict.insert(key, value);
-            }
-
-            dict
-        }
-    }
-
-    pub fn from_tuples(tuples: Vec<(K, V)>) -> Dictionary<K, V> {
-        if tuples.is_empty() {
-            panic!("Cannot create a zero-sized vec");
-        }
-        let mut dict: Dictionary<K, V> = Dictionary::new_sized(tuples.len());
-
-        for (key, value) in tuples {
-            dict.insert(key, value);
-        }
-
-        dict
-    }
-
-    pub fn size(&self) -> usize {
-        self.size
-    }
-
-    pub fn capacity(&self) -> usize {
-        self.capacity
-    }
-
-    //Checks if a resize is needed before inserting the new item, resizes if needed
-    pub fn insert(&mut self, key: K, value: V) {
-        self.size += 1;
-        if 2 * (self.capacity/3) < self.size {
-            self.resize();
-        }
-        let hash = self.get_hash(&key);
-        self.force_insert(key, value, hash);
-    }
-
-    fn resize(&mut self) {
-        self.capacity *= 2;
-        let _table = self.table.clone();
-        self.table = vec![Bucket::Empty; self.capacity];
-        for entry in _table.iter() {    
-            if let Bucket::Entry(d) = entry.clone() {
-                self.force_insert(d.0, d.1, d.2);
-            }
-        }
-    }
-
-    // Inserts new items without regard for size of the dict, it is separated from 
-    // the insert() function to prevent recursion on resizing. 
-    fn force_insert(&mut self, key: K, value: V, key_hash: usize) {
-        let mut index = (key_hash % self.capacity) as usize;
-        const PERTURB_SHIFT: u8 = 5;
-        let mut perturb: usize = key_hash;
-
-        loop {
-            let current: Bucket<K, V> = self.table.get(index).unwrap().clone();
-
-            match current {
-                Bucket::Entry(d) => {
-                    if d.0 == key {
-                        self.table[index] = Bucket::Entry((d.0, value, d.2, index));
-                        break;
-                    } else {
-                        perturb >>= PERTURB_SHIFT;
-                        index = ((5*index) + 1 + perturb) % self.capacity as usize;
-                        continue
-                    }
-                },
-
-                _ => {
-                    self.table[index] = Bucket::Entry((key, value, key_hash, index));
-                    break;
-                }
-            };
-        }
-    }
 
     /* Performs a lookup using almost the exact same algorithm as insertion
      * Returns an Some(value) if the key exists, and None otherwise
+     * Probing uses two numbers that are used in the calculation of each index: perturb and PERTURB_SHIFT
+     * perturb is used in the calculating of the "random" probing and is shifted to the right by PERTURB_SHIFT
+     * bits after every iteration in the probing
      */
     fn lookup(&self, key: &K) -> Option<(K, V, usize)> { 
         let key_hash: usize = self.get_hash(&key);
@@ -187,6 +93,97 @@ impl<K: Clone + Hash + PartialEq, V: Clone> Dictionary<K, V>{
         }
     }
 
+    // Inserts new items without regard for size of the dict, it is separated from 
+    // the insert() function to prevent recursion on resizing. 
+    fn force_insert(&mut self, key: K, value: V, key_hash: usize) {
+        let mut index = (key_hash % self.capacity) as usize;
+        const PERTURB_SHIFT: u8 = 5;
+        let mut perturb: usize = key_hash; 
+
+        loop {
+            let current: Bucket<K, V> = self.table.get(index).unwrap().clone();
+
+            match current {
+                Bucket::Entry(d) => {
+                    if d.0 == key {
+                        self.table[index] = Bucket::Entry((d.0, value, d.2, index));
+                        break;
+                    } else {
+                        perturb >>= PERTURB_SHIFT;
+                        index = ((5*index) + 1 + perturb) % self.capacity as usize;
+                        continue
+                    }
+                },
+
+                _ => {
+                    self.table[index] = Bucket::Entry((key, value, key_hash, index));
+                    break;
+                }
+            };
+        }
+    }
+
+    // Empties the table and makes a table twice the size, then reinserts all the entries
+    fn resize(&mut self, new_capacity: usize) {
+        self.capacity = new_capacity;
+        let _table = self.table.clone();
+        self.table = vec![Bucket::Empty; self.capacity];
+        for entry in _table.iter() {    
+            if let Bucket::Entry(d) = entry.clone() {
+                self.force_insert(d.0, d.1, d.2);
+            }
+        }
+    }
+
+    //Checks if a resize is needed before inserting the new item, resizes if needed
+    pub fn insert(&mut self, key: K, value: V) {
+        self.size += 1;
+        if 2 * (self.capacity/3) < self.size { // Double capacity if 2/3 full or more
+            self.resize(2 * self.capacity);
+        }
+        let hash = self.get_hash(&key);
+        self.force_insert(key, value, hash);
+    }
+
+    //Returns a Result::Err if the vectors are different sizes
+    pub fn from_vecs(mut key_vec: Vec<K>, mut value_vec: Vec<V>) -> Dictionary<K, V> {
+        if key_vec.len() != value_vec.len() {
+            panic!("Differently sized vecs");
+        } else if key_vec.is_empty() {
+            panic!("Cannot create a zero-sized dict");
+        } else {
+            let dict: Dictionary<K, V> = with_capacity(key_vec.len()/2)*3 + 1);
+            for _ in 0..key_vec.len() {
+                let key = key_vec.pop().unwrap();
+                let value = value_vec.pop().unwrap();
+                dict.insert(key, value);
+            }
+
+            dict
+        }
+    }
+    
+    pub fn from_tuples(tuples: Vec<(K, V)>) -> Dictionary<K, V> {
+        if tuples.is_empty() {
+            panic!("Cannot create a zero-sized vec");
+        }
+        let mut dict: Dictionary<K, V> = Dictionary::with_capacity((tuples.len()*2)/3 + 1);
+
+        for (key, value) in tuples {
+            dict.insert(key, value);
+        }
+
+        dict
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
     pub fn get(&self, key: &K) -> Result<V, String> {
        match self.lookup(key) {
            Some(v) => Ok(v.1),
@@ -194,14 +191,23 @@ impl<K: Clone + Hash + PartialEq, V: Clone> Dictionary<K, V>{
        }
     }
 
-    pub fn remove (&mut self, key: &K) {
-        let value = self.lookup(key);
-        match value {
+    pub fn remove (&mut self, key: &K) -> Option<(K, V)>{
+        let output: Option<(K, V)>;
+        // If the key exists, remove it from the dictionary and add the key and value to the output
+        match self.lookup(key) {
             Some(v) => {
                 self.table[v.2] = Bucket::Tombstone;
+                self.size -= 1;
+                output = Some((v.0, v.1));
             },
-            None => panic!("Key is not in the dictionary")
+            None => {output = None;}
+        };
+
+        if self.size < self.capacity/3 { // If current size is less than 2/3 half capacity, aka less than 1/3 capacity
+            self.resize(self.capacity/2); 
         }
+
+        output
     }
 
     pub fn contains(&self, key: &K) -> bool {
@@ -213,13 +219,9 @@ impl<K: Clone + Hash + PartialEq, V: Clone> Dictionary<K, V>{
         key.hash(&mut s);
         s.finish() as usize
     }
+
     pub fn clear(&mut self) {
         *self = Dictionary::new();
-    }
-
-    fn get_non_empty(&self) -> impl Iterator<Item = &Bucket<K, V>> + '_ {
-        self.table.iter()
-            .filter(|v| match v { Bucket::Entry(_n) => true, _ => false })
     }
 
     // Returns a vector of keys contained in the dict
@@ -258,14 +260,15 @@ impl<K: Clone + Hash + PartialEq, V: Clone> Dictionary<K, V>{
 }
 
 impl<K, V> fmt::Display for Dictionary<K, V>
-    where K: Clone + Hash + fmt::Display + PartialEq + Eq,
-          V: Clone + fmt::Display {
+    where K: fmt::Display + Clone + Hash,
+          V: fmt::Display + Clone {
 
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut output_str = String::new();
         output_str.push_str("{");
 
-        for k in self.get_non_empty() {
+        for k in self.table.iter() // Iterate over all buckets containing an entry
+            .filter(|v| match v { Bucket::Entry(_n) => true, _ => false }) {
             if let Bucket::Entry(d) = k {
                 write!(output_str, "{}: {}, ", d.0, d.1)?;
             }
--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,14 @@ use dictionary::Dictionary;
  * 5: 0
  * }
  */
+#[allow(dead_code)]
 fn create_dict() -> Dictionary<u8, u8> {
     let tuples: Vec<(u8, u8)> = vec![(1, 6), (2, 7), (3, 8),
                              (4, 9), (5, 0)];
     Dictionary::from_tuples(tuples)
 }
 
+#[allow(dead_code)]
 fn has_same_elements<T: PartialEq>(vec1: &Vec<T>, vec2: &Vec<T>) -> bool {
     for i in vec1 {
         if vec2.contains(i) {
@@ -41,14 +43,14 @@ mod tests{
 
     #[test]
     fn create_sized() {
-        let _d: Dictionary<u8, u8> = Dictionary::new_sized(16); 
+        let _d: Dictionary<u8, u8> = Dictionary::with_capacity(16); 
         assert_eq!(_d.capacity(), 16);
     }
 
     #[test]
     #[should_panic]
     fn zero_sized_dict() {
-        let _d: Dictionary<u8, u8> = Dictionary::new_sized(0);
+        let _d: Dictionary<u8, u8> = Dictionary::with_capacity(0);
     }
 
     #[test]
@@ -93,7 +95,7 @@ mod tests{
 
     #[test]
     fn lookup() {
-        let _d: Dictionary<u8, u8> = create_dict();
+        let _d = create_dict();
 
         assert_eq!(_d.get(&1).unwrap(), 6);
     }
@@ -107,8 +109,14 @@ mod tests{
     }
 
     #[test]
+    fn size() {
+        let _d = create_dict();
+        assert_eq!(_d.size(), 5);
+    }
+
+    #[test]
     fn resize() {
-        let mut _d: Dictionary<u8, u8> = Dictionary::new_sized(4);
+        let mut _d: Dictionary<u8, u8> = Dictionary::with_capacity(4);
         assert_eq!(_d.capacity(), 4);
         for i in 0..4{
             _d.insert(i, i);
@@ -119,22 +127,32 @@ mod tests{
 
     #[test]
     fn contains() {
-        let mut _d: Dictionary<u8, u8> = create_dict();
+        let mut _d = create_dict();
         
         assert!(_d.contains(&1));
     }
 
     #[test]
     fn remove() {
-        let mut _d: Dictionary<u8, u8> = create_dict();
-        _d.remove(&1);
+        let mut _d = create_dict();
+        let _r = _d.remove(&1);
         
-        assert!(! _d.contains(&1));
+        assert!((!_d.contains(&1)) &&
+        _r.is_some() &&
+        _r.unwrap() == (1, 6) &&
+        _d.size() == 4);
     }
     
+    #[test]
+    fn down_size() {
+        let mut _d = create_dict();
+        for i in 0..4{
+            _d.remove(&i);
+        }
+        assert_eq!(_d.capacity(), 5);
+    }
 
     #[test]
-    #[should_panic]
     fn remove_panic() { 
         let mut _d: Dictionary<u8, u8> = Dictionary::new();
         _d.remove(&1);
@@ -142,7 +160,7 @@ mod tests{
 
     #[test]
     fn keys() {
-        let _d: Dictionary<u8, u8> = create_dict();
+        let _d = create_dict();
         let expected_keys: Vec<u8> = vec![1, 2, 3, 4, 5];
         let keys = _d.keys().into_iter().map(|x| *x).collect(); 
         assert!(has_same_elements(&keys, &expected_keys));
@@ -150,7 +168,7 @@ mod tests{
     
     #[test]
     fn values() {
-        let _d: Dictionary<u8, u8> = create_dict();
+        let _d = create_dict();
         let expected_values: Vec<u8> = vec![6, 7, 8, 9, 0];
         let values = _d.values().into_iter().map(|x| *x).collect();
         assert!(has_same_elements(&values, &expected_values));
@@ -163,5 +181,11 @@ mod tests{
         let _d: Dictionary<u8, u8> = Dictionary::from_tuples(_t);
         let expected_items = _d.items().into_iter().map(|x| (*x.0, *x.1)).collect();
         assert!(has_same_elements(&expected_items, &tuples));
+    }
+
+    #[test]
+    fn clear() {
+        let mut _d = create_dict();
+        _d.clear();
     }
 }
